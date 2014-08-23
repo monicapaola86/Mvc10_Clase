e244ef8dfa959db4b841c9232d1e61ee4e991947
monicapaola86 <nez.paola8@gmail.com>
2014-08-23 11:57:55 -0400

    Mvc4_Clase10

---
+++ MvcApplication2/Models/Mvc4tres.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication2.Models
+{
+    public class Mvc4tres: DbContext
+    {
+        public Mvc4tres():base("name=CONNN")
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+        public DbSet<RestaurantReview> Reviews { get; set; }
+    }
+}
---
+++ MvcApplication2/Models/Restaurant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication2.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Ciudad { get; set; }
+        public string Pais { get; set; }
+        public ICollection<RestaurantReview> Reviews { get; set; }
+
+    }
+}
